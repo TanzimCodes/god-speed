51f52ea0886158c1454e3094045348bb91c569e4
tanzim <tanzim@softwaredeveloper.com>
2024-10-19 13:40:53 -0400

    Update page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,8 +3,10 @@ export default function Header() {
     <div className="border-2 border-rose-200 absolute top-1/2 left-1/2 text-center">
       Login
       <p>Username</p>
+      <input type="text" className="border-2"></input>
       <p>Password</p>
       <input type="text" className="border-2"></input>
+      <p></p>
       <button className="border-2 bg-green-600 text-cyan-50 m-auto">Login</button>
     </div>
   );
